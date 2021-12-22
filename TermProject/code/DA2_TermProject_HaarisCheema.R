7c7553cf340db454b360a4f0e1ec80b26f6baf1d
Haaris Afzal Cheema <Cheema_Haaris@student.ceu.edu>
2021-12-22 13:14:31 +0100

    Adding final R file and Markdown

--- TermProject/code/DA2_TermProject_HaarisCheema.R
+++ TermProject/code/DA2_TermProject_HaarisCheema.R
@@ -9,11 +9,16 @@ library(raster)
 library(tidyverse)
 library(fixest)
 library(lspline)
-
-# seq etc in graphs
-# cities data - try to find online for fpl team
+library(corrplot)
+library(car)
+library(estimatr)
+library(gridExtra)
 
 ##################################################################################################################################
+
+# Clearing the environment
+
+rm(list= ls())
 
 ## Loading and combining the weather data
 
@@ -134,8 +139,15 @@ final_df <- inner_join(football, weather, by=c("date","City"))
 
 final_df$rain <- ifelse(final_df$precip > 0 , 1,0)
 
-############################################################################################################################
+# Viewing the data at a very holistic level
 
+summary(final_df)
+
+glimpse(final_df)
+
+table(is.na(final_df))
+
+############################################################################################################################
 ## Summary statistics and Exploratory Data Analysis 
 
 # Defining the 95th percentile function
@@ -159,128 +171,158 @@ theme_set(theme_custom())
 
 # Distribution for total goals 
 
-ggplot(data = final_df) + 
-  geom_histogram( aes( x = tgc ) , fill = 'blue', col = 'black', binwidth = 1, alpha = 0.5   )  +
-    labs( x = "Total goal count", y = "Relative Frequency")
+h1 <- ggplot(data = final_df) + 
+        geom_histogram( aes( x = tgc ) , fill = 'blue', col = 'black', binwidth = 1, alpha = 0.5)  +
+          labs( x = "Total goal count", y = "Relative Frequency")
 
 # Distribution for daily precipitation (in mm) - try logs? - fix start
 
-ggplot(data = final_df) + 
-  geom_histogram(aes(x = rain) , fill = 'blue', col = 'black', binwidth = 0.5, alpha = 0.5) +
-    labs( x = "Rain", y = "Relative Frequency")
+h2 <- ggplot(data = final_df) + 
+       geom_histogram(aes(x = rain) , fill = 'blue', col = 'black', binwidth = 0.5, alpha = 0.5) +
+        labs( x = "Rain", y = "Relative Frequency")
 
 # Distribution for number of corners
 
-ggplot(data = final_df) + 
-  geom_histogram( aes( x = cnr ) , fill = 'blue', col = 'black', binwidth = 2, alpha = 0.5) +
-    labs( x = "Number of Corners", y = "Relative Frequency")
+h3 <- ggplot(data = final_df) + 
+        geom_histogram( aes( x = cnr ) , fill = 'blue', col = 'black', binwidth = 2, alpha = 0.5) +
+         labs( x = "Number of Corners", y = "Relative Frequency")
 
 
 # Distribution for number of yellow cards
 
-ggplot(data = final_df) + 
-  geom_histogram( aes( x = yc ) , fill = 'blue', col = 'black', binwidth = 1, alpha = 0.5 ) +
-    labs( x = "Number of Yellow Cards", y = "Relative Frequency")
+h4 <- ggplot(data = final_df) + 
+        geom_histogram( aes( x = yc ) , fill = 'blue', col = 'black', binwidth = 1, alpha = 0.5) +
+          labs( x = "Number of Yellow Cards", y = "Relative Frequency")
 
 
 # Distribution for number of red cards
 
-ggplot(data = final_df) + 
-  geom_histogram( aes( x = rc ) , fill = 'blue', col = 'black', binwidth = 0.5, alpha = 0.5 ) +
-    labs( x = "Number of Red Cards", y = "Relative Frequency")
+h5 <- ggplot(data = final_df) + 
+        geom_histogram( aes( x = rc ) , fill = 'blue', col = 'black', binwidth = 0.5, alpha = 0.5) +
+          labs( x = "Number of Red Cards", y = "Relative Frequency")
 
 
 # Distribution for number of shots in a match 
 
-ggplot(data = final_df) + 
-  geom_histogram( aes( x = shots ) , fill = 'blue',col = 'black', binwidth = 3 , alpha = 0.5) +
-    labs( x = "Total Number of Shots", y = "Relative Frequency")
+h6 <- ggplot(data = final_df) + 
+        geom_histogram( aes( x = shots ) , fill = 'blue',col = 'black', binwidth = 3 , alpha = 0.5) +
+          labs( x = "Total Number of Shots", y = "Relative Frequency")
 
 
 # Distribution for number of shots on target in a match
 
-ggplot(data = final_df) + 
-  geom_histogram( aes( x = shots_t) , fill = 'blue',col = 'black', binwidth =  3 , alpha = 0.5) +
-    labs( x = "Total Number of Shots on Target", y = "Relative Frequency")
+h7 <- ggplot(data = final_df) + 
+        geom_histogram( aes( x = shots_t) , fill = 'blue',col = 'black', binwidth =  3 , alpha = 0.5) +
+          labs( x = "Total Number of Shots on Target", y = "Relative Frequency")
 
+grid.arrange(h1, h2, h3, h4, h5, h6, h7, ncol = 2)
 
 ###########################################################################################################################
 
 ## Checking association between tgc and all independent vars through lowess curves
 
-ggplot(data = final_df, aes(x= rain, y= tgc)) +
+a1 <- ggplot(data = final_df, aes(x= rain, y= tgc)) +
   geom_smooth(method="loess", formula = y ~ x)  +
     geom_point(col="red") +
       annotate("text", x = 0.5, y = 4, label = "No obvious association observed", size=4)+
-  #scale_x_continuous(expand = c(0.01,0.01), limits = c(1,5), breaks = seq(1,5,1))+
-  #scale_y_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.1)) +
-          labs(x = "Rain",y = "Total Number of Goals")
+        scale_x_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.5))+
+          scale_y_continuous(expand = c(0.01,0.01), limits = c(0,8), breaks = seq(0,8,2)) +
+            labs(x = "Rain",y = "Total Number of Goals")
 
 
-ggplot(data = final_df, aes(x= cnr, y= tgc)) +
+
+a2 <- ggplot(data = final_df, aes(x= cnr, y= tgc)) +
   geom_smooth(method="loess", formula = y ~ x) +
     geom_point(col="red") +
-      annotate("text", x = 10, y = 4.5, label = "Linear negative association observed", size=4)+
-  #scale_x_continuous(expand = c(0.01,0.01), limits = c(1,5), breaks = seq(1,5,1))+
-  #scale_y_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.1)) +
+      annotate("text", x = 10, y = 4.5, label = "Linear negative association observed", size=4) +
+        scale_x_continuous(expand = c(0.01,0.01), limits = c(2,22), breaks = seq(2,22,4)) +
+          scale_y_continuous(expand = c(0.01,0.01), limits = c(0,8), breaks = seq(0,8,2)) +
             labs(x = "Number of Corners",y = "Total Number of Goals")
 
 
-ggplot(data = final_df, aes(x= yc, y= tgc)) +
+a3 <- ggplot(data = final_df, aes(x= yc, y= tgc)) +
   geom_smooth(method="loess", formula = y ~ x) +
     geom_point(col= "red") +
-      annotate("text", x = 4.5, y = 4.5, label = "Changes in slope direction, need to add knots at yc = 4 and 6 for lsp", size=4)+
-  #scale_x_continuous(expand = c(0.01,0.01), limits = c(1,5), breaks = seq(1,5,1))+
-  #scale_y_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.1)) +
+      annotate("text", x = 4.5, y = 4.5, label = "Changes in slope direction, need to add knots at yc = 4 and 6 for lsp", size=3)+
+        scale_x_continuous(expand = c(0.01,0.01), limits = c(0,9), breaks = seq(0,9,3))+
+          scale_y_continuous(expand = c(0.01,0.01), limits = c(0,8), breaks = seq(0,8,2)) +
             labs(x = "Number of Yellow Cards",y = "Total Number of Goals") 
   
   
-# Error 
-ggplot(data = final_df, aes(x= rc, y= tgc)) +
+a4 <- ggplot(data = final_df, aes(x= shots, y= tgc)) +
   geom_smooth(method="loess", formula = y ~ x) +
-  #scale_x_continuous(expand = c(0.01,0.01), limits = c(1,5), breaks = seq(1,5,1))+
-  #scale_y_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.1)) +
-  labs(x = "Number of Red Cards",y = "Total Number of Goals") 
-  
- 
-ggplot(data = final_df, aes(x= shots, y= tgc)) +
-  geom_smooth(method="loess", formula = y ~ x) +
-    geom_point( col = "red") +
-      annotate("text", x = 15, y = 3.5, label = "More or less linear positive association", size=4) +
-  #scale_x_continuous(expand = c(0.01,0.01), limits = c(1,5), breaks = seq(1,5,1))+
-  #scale_y_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.1)) +
-            labs(x = "Total Number of Shots",y = "Total Number of Goals ") 
+  geom_point( col = "red") +
+  annotate("text", x = 16, y = 3.5, label = "More or less linear positive association", size=4) +
+  scale_x_continuous(expand = c(0.01,0.01), limits = c(7,34), breaks = seq(7,34,3))+
+  scale_y_continuous(expand = c(0.01,0.01), limits = c(0,8), breaks = seq(0,8,2)) +
+  labs(x = "Total Number of Shots",y = "Total Number of Goals ")  
   
 
-ggplot(data = final_df, aes(x = shots_t, y = tgc)) +
+a5 <- ggplot(data = final_df, aes(x = shots_t, y = tgc)) +
   geom_smooth(method="loess", formula = y ~ x) +
     geom_point(col = "red") +
       annotate("text", x = 16, y = 5.7, label = "Change in slope direction, need to add knot here for lsp", size=4) +
-  #scale_x_continuous(expand = c(0.01,0.01), limits = c(1,5), breaks = seq(1,5,1))+
-  #scale_y_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1,0.1)) +
+        scale_x_continuous(expand = c(0.01,0.01), limits = c(2,22), breaks = seq(2,22,4))+
+         scale_y_continuous(expand = c(0.01,0.01), limits = c(0,8), breaks = seq(0,8,2)) +
             labs(x = "Shots on target",y = "Total Number of Goals") 
+
+grid.arrange(a1,a2,a3,a4,a5, ncol = 2)
 
 # Creating squared term for shots_t based on pattern of association
 
 final_df$shots_t_sq <- (final_df$shots_t)^2
 
+#################################################################################################################################
+## Examining the correlation coefficients between the causal variable and the conditioning variables
+
+# Subsetting the data for the correlations
+
+cor_data <- (final_df[,c(11,4:8)])
+
+# Creating a correlation matrix
+
+res <- cor(cor_data)
+
+# Displaying the matrix with 2 decimal places
+
+round(res, 2)
+
+# Visualizing the correlation
+
+corrplot(res, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+
+# Independent variable regressions
+
+model1 <- lm(rain ~ cnr + yc + rc + shots + shots_t , data = final_df)
+model2 <- lm(cnr ~ rain + yc + rc + shots + shots_t , data = final_df)
+model3 <- lm(yc ~ rain + cnr +rc +shots + shots_t, data = final_df)
+model4 <- lm(rc ~ rain + cnr + yc + shots + shots_t, data = final_df)
+model5 <- lm(shots ~ rain + cnr + yc + rc + shots_t, data = final_df)
+model6 <- lm(shots_t ~ rain + cnr + yc + rc + shots, data = final_df)
+
+#vif(model1)
+#vif(model2)
+#vif(model3)
+#vif(model4)
+#vif(model5)
+#vif(model6)
+
+
+
 #################################################################################################################################  
+
 ## Regressions
 
-# Regressing x on confounding variables
+# 5 models
 
-xreg <- feols( rain ~ cnr + yc + rc + shots + shots_t, data = final_df, vcov = 'hetero')
-xreg
+reg1 <- feols( tgc ~ rain + cnr + yc + rc + shots + shots_t , data = final_df, vcov = 'hetero')
 
-# Main Regressions
+reg2 <- feols( tgc ~ rain + cnr + yc + rc  + shots_t  , data = final_df , vcov = 'hetero' )
 
-reg1 <- feols( tgc ~ rain + cnr + yc + shots + shots_t , data = final_df, vcov = 'hetero')
+reg3 <- feols( tgc ~ rain + cnr + lspline(yc,c(4,6)) + rc + shots + lspline(shots_t,17)  , data = final_df , vcov = 'hetero' )
 
-reg2 <- feols( tgc ~ rain + cnr + yc + rc + shots + shots_t  , data = final_df , vcov = 'hetero' )
-
-reg3 <- feols( tgc ~ rain + cnr + lspline(yc,c(4,6)) + shots + lspline(shots_t,17)  , data = final_df , vcov = 'hetero' )
-
-reg4 <- feols( tgc ~ rain + cnr + lspline(yc,c(4,6)) + rc + shots + lspline(shots_t,17)  , data = final_df , vcov = 'hetero' )
+reg4 <- feols( tgc ~ rain + cnr + lspline(yc,c(4,6)) + rc  + lspline(shots_t,17)  , data = final_df , vcov = 'hetero' )
 
 reg5 <- feols( tgc ~ rain + cnr + lspline(yc,c(4,6)) + rc + shots + shots_t_sq  , data = final_df , vcov = 'hetero' )
 
@@ -289,4 +331,20 @@ reg5 <- feols( tgc ~ rain + cnr + lspline(yc,c(4,6)) + rc + shots + shots_t_sq  
 
 etable(reg1, reg2,reg3, reg4, reg5)
 
+# Inference Testing for chosen model
+
+summary(reg4)
+
+
+#################################################################################################################################
+
+## Rough Work - Feel free to ignore this part. 
+
+# Splines - counts in each category
+
+final_df$check1 <- ifelse(final_df$shots_t < 17, 0, 1)
+
+table(final_df$check1)  # only 4.2 % of matches have shots > 17
+
+final_df %>% filter(final_df$yc >= 6) # 226 obs with less than 4 yc, 109 obs for b/w 4 and 6, 45 obs with more than 6
 
